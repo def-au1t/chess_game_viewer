81844f369f0297773a0d70df3ce2ec28500d1252
Jck-eN <jaceknit.rac@gmail.com>
2020-03-22 16:20:06 +0100

    initial commit

---
+++ chess_game_viewer/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for chess_game_viewer project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chess_game_viewer.settings')
+
+application = get_wsgi_application()
