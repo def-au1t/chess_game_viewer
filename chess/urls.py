0ed168374c1bffd4d8899e3a4e9d372a5e8a14f2
rivit98 <albert98g@gmail.com>
2020-04-14 22:52:03 +0200

    Reverted to pagination and default views

--- chess/urls.py
+++ chess/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import GamesList, GameDetails, TournamentsList, TournamentDetails, GameDetailsTest
+from .views import GamesList, GameDetails, TournamentsList, TournamentDetails
 
 urlpatterns = [
     path('', GamesList.as_view(), name='index'),
-    path('game/<int:pk>', GameDetails.as_view(), name='game'),  # TODO: this will be useless after converting to dynamic page
+    path('game/<int:pk>', GameDetails.as_view(), name='game'),
     path('tournaments', TournamentsList.as_view(), name='tournaments'),
     path('tournament/<int:pk>', TournamentDetails.as_view(), name='tournament'),
-    path('game_details/<int:pk>', GameDetailsTest.as_view(), name='game_details'),  # TODO: maybe rename to 'game' endpoint
 ]
