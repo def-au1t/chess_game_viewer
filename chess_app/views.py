9ef49b8f712a9774c1372b022a675ad037cdb6c3
rivit98 <albert98g@gmail.com>
2020-05-06 01:20:34 +0200

    tournament filtering - base version * 5 filters

--- chess_app/views.py
+++ chess_app/views.py
@@ -7,36 +7,63 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.sessions.middleware import SessionMiddleware
 from .getdata import get_player_data
+from django import  forms
 
+class TournamentsList(ListView, forms.Form):
+    t_name = forms.CharField(max_length=256)
+    t_date = forms.DateField()
+    t_city = forms.CharField(max_length=256)
+    t_gametime = forms.IntegerField()
+    t_type = forms.Select()
 
-class TournamentsList(ListView):
     model = Tournament
-    template_name = "../templates/tournaments.html"
-    paginate_by = 5
-    ordering = ['-date']
+    template_name = "../templates/tournament_list.html"
+    paginate_by = 1  # TODO: change this
+
+    def get_queryset(self):
+        queryset = Tournament.objects.all()
+        filters = self.request.GET
+
+        if filters.get("t_name"):
+            queryset = queryset.filter(name__icontains=filters["t_name"])
+
+        if filters.get("t_date"):
+            queryset = queryset.filter(date__iexact=filters["t_date"])
+
+        if filters.get("t_city"):
+            queryset = queryset.filter(city__icontains=filters["t_city"])
+
+        if filters.get("t_gametime"):
+            queryset = queryset.filter(time__exact=filters["t_gametime"])
+
+        if filters.get("t_type"):
+            queryset = queryset.filter(type__exact=filters["t_type"])
+
+        return queryset.order_by('-date')
 
 
-class TournamentDetails(DetailView, MultipleObjectMixin):
+class TournamentDetails(DetailView, MultipleObjectMixin):  # TODO: MultipleObjectMixin - is that necessary?
     model = Tournament
     template_name = "../templates/tournament.html"
+
     # paginate_by = 5
     # ordering = ['-id']
 
     def get_context_data(self, **kwargs):
         games = list(Game.objects.filter(tournament_id=self.object.id).order_by("-id"))
-        rounds = set()
-        for game in games:
-            rounds.add(game.round)
+        rounds = set(map(lambda g: g.round, games))
 
         games_grouped = dict()
         for single_round in rounds:
             games_grouped[single_round] = [game for game in games if game.round == single_round]
+
         context = super(TournamentDetails, self).get_context_data(object_list=games_grouped.items(), **kwargs)
         return context
 
+
 class GamesList(ListView):
     model = Game
-    template_name = "../templates/index.html"
+    template_name = "../templates/game_list.html"
     paginate_by = 10
     ordering = ['id']
 
@@ -47,19 +74,20 @@ class GameDetails(DetailView):
 
     def get_context_data(self, **kwargs):
         similar_games = list(Game.objects.filter(tournament_id=self.object.tournament_id).order_by("round"))
-        type='tournament'
+
+        game_type = 'tournament'
         if len(similar_games) < 2:
             similar_games = list(Game.objects.exclude(id=self.object.id).order_by('?')[:5])
             similar_games[0] = self.object
-            type='random'
+            game_type = 'random'
+
         current_game_id = self.object.id
         context = super(GameDetails, self).get_context_data(
             similar_games=similar_games,
             current_game_id=current_game_id,
-            type=type,
+            type=game_type,
             **kwargs)
         return context
-
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -83,4 +111,3 @@ def parse_pgn(request):
     else:
         formset = PgnFormSet(queryset=PGN.objects.none(), initial=[{'pgn': x} for x in pgns])
         return render(request, 'parser.html', {'formset': formset})
-
